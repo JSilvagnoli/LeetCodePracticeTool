9b3631a8a1374940d5e6a7b717324940885ea1a2
Joshua Silvagnoli <silvagnolijoshua@gmail.com>
2026-08-25 18:07:32 -0400

    Add functions to fetch LeetCode question content and metadata.

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ import requests
 
 leetcode_url = "https://leetcode.com/graphql"
 
-def getAllQuestions():
+headers = {
+    "User-Agent": "Mozilla/5.0",
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Referer": "https://leetcode.com/",
+    "Origin": "https://leetcode.com",
+}
+
+def get_all_questions():
     all_questions = []
 
     limit = 50
@@ -31,59 +39,169 @@ def getAllQuestions():
                     name
                     slug
                 }
+                content
             }
         }
     }
     """
 
-    headers = {
-        "User-Agent": "Mozilla/5.0",
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Referer": "https://leetcode.com/",
-        "Origin": "https://leetcode.com",
+    while True:
+        variables = {
+            "categorySlug": "",
+            "skip": skip,
+            "limit": limit,
+            "filters": {}
+        }   
+
+        response = requests.post(
+            leetcode_url,
+            headers=headers,
+            json={
+                "query": query,
+                "variables": variables,
+                "operationName": "problemsetQuestionList",
+            },
+            timeout=30,
+        )
+
+        response.raise_for_status()
+
+        result = response.json()
+
+        question_data = result["data"]["problemsetQuestionList"]
+
+        total_num = question_data["totalNum"]
+        print(total_num)
+
+        questions = question_data["data"]
+
+        all_questions.extend(questions)
+
+        skip += limit
+        if skip >= total_num:
+            break
+
+        return all_questions
+
+def get_solution():
+    query = """
+        query problemsetQuestionList(
+        $categorySlug: String,
+        $limit: Int,
+        $skip: Int,
+        $filters: QuestionListFilterInput
+    ) {
+        problemsetQuestionList: questionList(
+            categorySlug: $categorySlug
+            limit: $limit
+            skip: $skip
+            filters: $filters 
+        ) {
+            total: totalNum
+            questions: data {
+                frontendQuestionId: questionFrontendId
+                title
+                titleSlug
+                difficulty
+                paidOnly: isPaidOnly
+                hasSolution
+                hasVideoSolution
+            }
+        }
     }
+    """
 
     while True:
-      variables = {
-          "categorySlug": "",
-          "skip": skip,
-          "limit": limit,
-          "filters": {}
-      }   
+        variables = {
+            "categorySlug": "",
+            "skip": 0,
+            "limit": 1,
+            "filters": {
+                "searchKeywords": "28"
+            }
+        }   
 
-      response = requests.post(
-          leetcode_url,
-          headers=headers,
-          json={
-              "query": query,
-              "variables": variables,
-              "operationName": "problemsetQuestionList",
-          },
-          timeout=30,
-      )
+        response = requests.post(
+            leetcode_url,
+            headers=headers,
+            json={
+                "query": query,
+                "variables": variables,
+                "operationName": "problemsetQuestionList",
+            },
+            timeout=30,
+        )
 
-      response.raise_for_status()
+        response.raise_for_status()
 
-      result = response.json()
+        result = response.json()
 
-      question_data = result["data"]["problemsetQuestionList"]
+        return result["data"]["problemsetQuestionList"]["questions"][0]["titleSlug"]
 
-      total_num = question_data["totalNum"]
-      questions = question_data["data"]
+def get_question_details(title_slug):
+    query = """
+    query questionData($titleSlug: String!) {
+        question(titleSlug: $titleSlug) {
+            questionId
+            questionFrontendId
+            title
+            titleSlug
+            content
+            isPaidOnly
+            difficulty
 
-      all_questions.extend(questions)
+            similarQuestions
 
-      skip += limit
-      if skip >= total_num:
-          break
+            topicTags {
+                name
+                slug
+            }
 
-    return all_questions
+            codeSnippets {
+                lang
+                langSlug
+                code
+            }
 
+            hints
+
+            solution {
+                id
+                canSeeDetail
+            }
+
+            exampleTestcases
+            metaData
+        }
+    }
+    """
+
+    variables = {
+        "titleSlug": title_slug
+    }
+
+    response = requests.post(
+        leetcode_url,
+        headers=headers,
+        json={
+            "query": query,
+            "variables": variables,
+            "operationName": "questionData",
+        },
+        timeout=30,
+    )
+
+    response.raise_for_status()
+
+    return response.json()
 
 def main():
-    data = getAllQuestions()
-    print(data)
+    #data = getAllQuestions()
+    solution_data = get_solution()
+    print(solution_data)
+    solution = get_question_details(solution_data)
+    print(solution)
+    #print(data)
 
 
 if __name__ == "__main__":
